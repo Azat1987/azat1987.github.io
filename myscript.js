b7682818f9f6f26194075154068424d1e53e4403
Azat1987 <50105118+Azat1987@users.noreply.github.com>
2019-04-29 14:50:08 +0300

    Zagruzka pervih failov
    Zagruzil index.html i instagram.css

---
+++ myscript.js
@@ -0,0 +1,53 @@
+/*alert ('Poka!');
+var words;
+words='Dobrii den!';
+alert (words);
+alert (words);*/
+/*var year = prompt ('какой следующий год?', '');
+alert (year);
+alert (year + ' ' + year);
+var admin= confirm ("Vi administrator?");*/
+/*var year=prompt ("Kogda Olimpiada?", "");
+if (year !=2020){
+alert("Nepravil'no!");
+}
+else {
+  alert ("Pravil'no";
+}*/
+/*var year = prompt ("Kogda Olimpiada?", "");
+if (year < 2020) {
+alert ("Rano!");
+}
+else if  (year > 2020) {
+  alert ("Pozdno");
+} else {
+  alert ("Pravil'no");
+}*/
+/*var i = 0;
+while (i<3) {
+  alert (i);
+  i++;
+}*/
+/*var i=7;
+do {
+  alert (i);
+  i++;
+} while (i<3);*/
+/*while (year!=2020) {
+var year = prompt ("Kogda Olimpiada?", "");
+if (year < 2020) {
+alert ("Rano!");
+}
+else if  (year > 2020) {
+  alert ("Pozdno");
+} else {
+  alert ("Pravil'no");
+}
+}*/
+/*var i;
+for (i=0;i<3;i++) {
+alert (i);
+}*/
+function showMessage () {
+  alert ("Eto moya fuckciya");
+}
